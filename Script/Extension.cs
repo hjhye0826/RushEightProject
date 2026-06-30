74c1009acc6dd8f95ba2d62b02b4bbca4b6d45d1
hjhye0826 <99808433+hjhye0826@users.noreply.github.com>
2026-06-30 17:37:39 +0900

    총 소요시간 구하는 로직 추가

---
+++ Script/Extension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RushEightProject
+{
+    public static class Extension
+    {
+        public static String TimeString(this int totalSeconds)
+        {
+            totalSeconds = Math.Max(totalSeconds, 0);
+
+            var days = Math.Floor(totalSeconds / (3600.0f * 24));
+            totalSeconds -= (int)(days * (3600.0f * 24));
+
+            var hours = Math.Floor(totalSeconds / 3600.0f);
+            totalSeconds -= (int)(hours * 3600.0f);
+
+            var minutes = Math.Floor(totalSeconds / 60.0f);
+            totalSeconds -= (int)(minutes * 60.0f);
+
+            StringBuilder Builder = new StringBuilder();
+            Builder.Length = 0;
+
+            if (days > 0)
+            {
+                Builder.Append(days);
+                Builder.Append("일 ");
+            }
+
+            if (hours > 0)
+            {
+                Builder.Append(hours);
+                Builder.Append("시간 ");
+            }
+
+            if (minutes > 0)
+            {
+                Builder.Append(minutes);
+                Builder.Append("분 ");
+            }
+
+            if (totalSeconds > 0)
+            {
+                Builder.Append(totalSeconds);
+                Builder.Append("초");
+            }
+
+            return Builder.ToString();
+        }
+    }
+}
--- Script/SubwayManager.cs
+++ Script/SubwayManager.cs
@@ -4,6 +4,8 @@ namespace RushEightProject
 {
     internal class SubwayManager
     {
+        public const int TransTime = 180;   // 초
+
         public Dictionary<string, Station> Stations = new Dictionary<string, Station>();
 
         public void InitSubwayInfo()
@@ -43,12 +45,60 @@ namespace RushEightProject
 
             if (start.Equals(end))
             {
-                Console.WriteLine("출발/도착역기 동일합니다. 다시 입력해 주세요");
+                Console.WriteLine("출발/도착역이 동일합니다. 다시 입력해 주세요");
                 SearchSubway();
                 return;
             }
 
-            // todo : 여기에 루트 찾기 로직 추가
+            SearchSubwayRoute(start, end);
+        }
+
+        private void SearchSubwayRoute(string start, string end)
+        {
+            var accTimes = new Dictionary<(string, int), int>();
+            var queue = new PriorityQueue<(string, int), int>();
+
+            var startStation = Stations[start];
+            var lines = startStation.Nodes.Select(d => d.LineNum).Distinct().ToList();
+
+            foreach (var line in lines)
+            {
+                queue.Enqueue((startStation.Name, line), 0);
+            }
+
+            while (queue.Count > 0)
+            {
+                var q = queue.TryDequeue(out var value, out var priority);
+
+                var stationName = value.Item1;
+                var lineNum = value.Item2;
+
+                if (stationName == end)
+                {
+                    Console.WriteLine($"총 소요 시간 : {priority.TimeString()}");
+                    return;
+                }
+
+                var station = Stations[stationName];
+
+                foreach (var nextStation in station.Nodes)
+                {
+                    var nextTime = priority + nextStation.Time;
+                    var transfer = lineNum != nextStation.LineNum;
+
+                    if (transfer)
+                    {
+                        nextTime += TransTime;
+                    }
+
+                    var key = (nextStation.Name, nextStation.LineNum);
+                    if (false == accTimes.TryGetValue(key, out var old) || nextTime < old)
+                    {
+                        accTimes[key] = nextTime;
+                        queue.Enqueue((nextStation.Name, nextStation.LineNum), nextTime);
+                    }
+                }
+            }
         }
 
         private bool SearchStation(string stationName)
